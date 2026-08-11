4fc6963f4837fd3d74190a616ab9a65c71174fe8
sarangkale111111-cpu <sarangkale111111@gmail.com>
2026-08-11 19:49:34 +0530

    Phase 0 & 1 Completed

---
+++ src/variables.ts
@@ -0,0 +1,23 @@
+let money:null = null;
+let age: number = 10;
+let name: string = "sarang";
+let ch: symbol = Symbol('$');
+let isGraduate: boolean = true;
+let population: bigint = 10000000000000000000000n;
+let dob: undefined = undefined;
+
+console.log(money, age, name, ch, isGraduate, population, dob)
+
+// let name1: any;
+let name1: unknown;
+
+if(typeof name1 === "string") name1.toUpperCase();
+
+function beep():never {
+  while(true){}
+}
+// call return aayega
+beep();
+
+console.log("hello")
+
